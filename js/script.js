2c04566b399e2650154022c5a8b8dbaab37c3d6b
Rahul Ramadani <rahulramadani08@gmail.com>
2025-08-30 10:23:23 +0700

    Update HTML structure and styles for Kedai Kopi Kayo; add images and enhance form validation

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ function greet() {
 function validateForm() {
     // Get the name input value
     let name = document.getElementById("name-input").value;
-
     // Validate the name input
     if (name === "") {
         // Show an alert if the name is empty
@@ -24,4 +23,23 @@ function validateForm() {
         // Show a thank you message if the name is valid
         alert("Thank you, " + name + ", for your message!");
     }
+
+    let email = document.getElementById("email-input").value;
+    if (email === "") {
+        alert("Email must be filled out");
+    }
+
+    let message = document.getElementById("message-input").value;
+    if (message === "") {
+        alert("Message must be filled out");
+    }
+        document.getElementById("myform").addEventListener("submit", function(e) {
+      e.preventDefault(); // Mencegah form submit secara default
+      var nama = document.getElementById("name-input").value;
+      var email = document.getElementById("email-input").value;
+      var pesan = document.getElementById("message-input").value;
+      var hasil = "Nama Anda :" + nama + ", Email Anda: " + email + ", Pesan Anda: " + pesan;
+      document.getElementById("hasilOutput").innerText = hasil; // Menampilkan hasil di elemen dengan id "hasilOutput"
+    });
 }
+
